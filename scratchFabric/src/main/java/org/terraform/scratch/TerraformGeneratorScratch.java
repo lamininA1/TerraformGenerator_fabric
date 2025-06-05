707d84c4a187b27641fc50ff1d263422583c19b5
lamininA1 <163475172+lamininA1@users.noreply.github.com>
2025-06-05 16:52:19 +0900

    Add scratch Fabric mod skeleton

---
+++ scratchFabric/src/main/java/org/terraform/scratch/TerraformGeneratorScratch.java
@@ -0,0 +1,10 @@
+package org.terraform.scratch;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TerraformGeneratorScratch implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("TerraformGenerator scratch mod initializing");
+    }
+}
